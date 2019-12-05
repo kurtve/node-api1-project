e737f0227b61767f9350a6f071912779f5af52fd
Kurt Van Etten <kurt@vanetten.us>
2019-12-04 21:06:03 -0500

    Server runs but does not do anything yet.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db.js');
 
-console.log('This is a test');
+// create an instance of express
+const app = express();
+
+// use middleware to parse request body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log("GET '/'");
+
+  res.json({ message: 'Welcome to our API' });
+});
+
+const port = 5000;
+const host = '127.0.0.1';
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
